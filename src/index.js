f86a38b201b80eafdfd98df9d2926c47811d1984
Viswanath Chidambaram <vc@abstractit.in>
2024-04-15 01:08:46 +0530

    Fixed the formatting.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import config from './config';
 import { getDataFetcher } from './dataFetcher';
 
+// #TODO: Extract this as a service.
+const formatToDo = ({ title, completed }) => ({ title, completed });
+
 const main = async () => {
 	const fetchData = getDataFetcher(config);
 
-	const data = await fetchData();
+	const todos = await fetchData();
+	const formattedTodos = todos.map(formatToDo);
 
 	// eslint-disable-next-line no-console
-	console.table(data);
+	console.table(formattedTodos);
 };
 
 main();
